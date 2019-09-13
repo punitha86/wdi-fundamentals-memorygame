8e28fefab34af6a383184fec0936c074b24cb8a4
wineverytime <punith.ariv@gmail.com>
2019-09-13 12:16:35 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped "+ cardOne);
-console.log("User flipped "+ cardTwo);
+
+
+var cards=["queen","queen","king","king"];
+var cardInPlay=[];
+var cardOne=cards[0];
+cardInPlay.push(cardOne);
+console.log("user flipped "+cardInPlay);
+var cardTwo=cards[2];
+cardInPlay.push(cardTwo);
+console.log("user flipped "+cardInPlay);
+if(cardInPlay.length===2){
+    if (cardInPlay[0]===cardInPlay[1]){
+        alert("You found a match!");
+    } else
+    alert("Sorry, try again.");
+}
